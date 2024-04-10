c90f9c586c8c39cbf0d750683adaa69695384c92
splengy <82328976+splengy@users.noreply.github.com>
2024-04-10 12:03:57 -0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,10 @@
+function startWorkout() {
+    alert("Starting workout...");
+    // Logic to start the workout will go here
+}
+
+function editPlan() {
+    alert("Editing exercise plan...");
+    // Logic to edit the exercise plan will go here
+}
 
